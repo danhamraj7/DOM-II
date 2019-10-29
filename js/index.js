9bd183b82dfaa536a6927fc55aac47fa65d29a4a
danhamraj <danhamraj@gmail.com>
2019-10-29 14:56:07 -0400

    Set up project and tested index page, added mouseove event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+//mouseover
+let pageHeader = document.querySelector(".logo-heading");
+pageHeader.addEventListener("mouseover", (event) => {
+    pageHeader.style.color = "red";
+    pageHeader.style.fontSize = "6rem";
+})
+// mouseleave
+let pageHeaderRev = document.querySelector(".logo-heading");
+pageHeader.addEventListener("mouseleave", (event) => {
+    pageHeader.style.color = "black";
+    pageHeader.style.fontSize = "4rem";
+});
