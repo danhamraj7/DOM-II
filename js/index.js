7db932691a9c1dc07e9a18f4cacccfd511398026
danhamraj <danhamraj@gmail.com>
2019-10-29 16:29:56 -0400

    Added event propation and wheel events

--- js/index.js
+++ js/index.js
@@ -11,3 +11,24 @@ pageHeader.addEventListener("mouseleave", (event) => {
     pageHeader.style.color = "black";
     pageHeader.style.fontSize = "4rem";
 });
+
+//wheel 
+let wheel = document.querySelector(".destination h4")
+
+wheel.addEventListener('wheel', (event) => {
+    alert("wheeling Fun In The Sun")
+});
+
+
+
+
+
+//event propagation for nav links.
+
+document.querySelectorAll(".nav-link").forEach(element => {
+
+    element.addEventListener("click", (event) => {
+        console.log("link stopped");
+        event.preventDefault();
+    })
+});
